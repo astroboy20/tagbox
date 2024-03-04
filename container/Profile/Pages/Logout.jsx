3c8a8a426b46d35844c1e3640dd70e3ecf35b210
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-03-04 11:29:03 +0100

    feat: completed the logout logic

--- container/Profile/Pages/Logout.jsx
+++ container/Profile/Pages/Logout.jsx
@@ -1,40 +1,63 @@
 import { useState } from "react";
-import Button from "react-bootstrap/Button";
 import { EditContainer } from "../Profile.style";
 import { Modal } from "@/components/Modal";
+import {
+  Actions,
+  Buttons,
+} from "@/container/Host/SingleEvent/SingleEvent.style";
+import { Button } from "@/components/Button/Button";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/router";
+import { reset } from "@/features/authSlice";
+import {toast} from "react-toastify"
 
 const Logout = () => {
   const [modalShow, setModalShow] = useState(false);
-  const [message, setMessage] = useState("");
-  const [showModal, setShowModal] = useState(false);
-  const [loading, setLoading] = useState(false)
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(reset());
+    toast.success("You have been successfully logged out");
+    router.push("/login");
+  };
   return (
     <>
-     <EditContainer>
-     <Button variant="primary" onClick={() => setModalShow(true)}>
-        Launch vertically centered modal
-      </Button>
-    
-     
-      {/* <Modal
-        show={modalShow}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        backdropClassName="backdrop"
-       
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+          margin:"20% auto"  
+        }}
       >
-        <Modal.Body>
-          <div>
-            <Button onClick={() => setModalShow(false)}>Close</Button>
-            <Button>Logout</Button>
-          </div>
-        </Modal.Body>
-      </Modal> */}
+        <Button variant="dark-button" onClick={() => setModalShow(true)}>
+          Logout
+        </Button>
 
-      <Button variant="primary" onClick={()=>setModalShow(true)}>Submit Response</Button>
-     </EditContainer>
-    
+        <Modal show={modalShow} onClose={() => setModalShow(false)}>
+          <>
+            <Actions className="actions">
+              <span>Are you sure you want to logout ?</span>
+              <Buttons className="buttons">
+                <button
+                  className={"white-btn"}
+                  onClick={() => setModalShow(false)}
+                >
+                  Cancel
+                </button>
+                <button className={"dark-button"} onClick={handleLogout}>
+                  {" "}
+                  Logout
+                </button>
+              </Buttons>
+            </Actions>
+          </>
+        </Modal>
+
+        <Button variant="primary" onClick={() => setModalShow(true)}>
+          Submit Response
+        </Button>
+      </div>
     </>
   );
 };
